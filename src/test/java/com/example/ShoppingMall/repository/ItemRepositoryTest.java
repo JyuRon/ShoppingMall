df8aa78c353283a3b3f00305d784fb006d05a470
JyuKa <yoyksks3@gmail.com>
2022-02-06 23:40:28 +0900

    Item Query Method 실습

--- src/test/java/com/example/ShoppingMall/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/ShoppingMall/repository/ItemRepositoryTest.java
@@ -2,6 +2,8 @@ package com.example.ShoppingMall.repository;
 
 import com.example.ShoppingMall.constant.ItemSellStatus;
 import com.example.ShoppingMall.entity.Item;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -18,6 +22,27 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @BeforeEach
+    public void createItemList(){
+        for (int i = 1; i <=10 ; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(100000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item saveItem = itemRepository.save(item);
+        }
+    }
+
+    @AfterEach
+    public void deleteItemList(){
+        itemRepository.deleteAll();
+    }
+
 
     @DisplayName("상품 저장 테스트")
     @Test
@@ -33,4 +58,36 @@ class ItemRepositoryTest {
         Item saveItem = itemRepository.save(item);
         System.out.println(saveItem.toString());
     }
+
+    @DisplayName("상품명 조회 테스트")
+    @Test
+    public void findByItemList(){
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        System.out.println("check");
+        then(itemList).hasSize(1);
+    }
+
+
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    @Test
+    public void findByItemNmOrItemDetail(){
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1","테스트 상품 상세 설명5");
+        then(itemList).hasSize(2);
+    }
+
+    @DisplayName("가격 LessThan 테스트")
+    @Test
+    public void findByPriceLessThen(){
+        List<Item> itemList = itemRepository.findByPriceLessThan(100005);
+        then(itemList).hasSize(4);
+    }
+
+    @DisplayName("가격 내림차순 조회 테스트")
+    @Test
+    public void findByPriceLessThanOrderByPriceDesc(){
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(100005);
+        then(itemList.get(0).getItemNm()).isEqualTo("테스트 상품4");
+    }
+
+
 }
